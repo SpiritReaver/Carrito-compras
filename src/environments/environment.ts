217d59cb5a02be40bd03057dc2db95e95484e0e6
SpiritReaver <54217513+SpiritReaver@users.noreply.github.com>
2022-04-25 20:45:21 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCk9KZ7N_M8y8UqMgLHpkGRLli8inBFXHM",
+    authDomain: "tpgrupalpps2020.firebaseapp.com",
+    databaseURL: "https://tpgrupalpps2020.firebaseio.com",
+    projectId: "tpgrupalpps2020",
+    storageBucket: "tpgrupalpps2020.appspot.com",
+    messagingSenderId: "219733044318",
+    appId: "1:219733044318:web:a8264e60400c93b45558cf",
+    measurementId: "G-PJHKS2KP1Q"
+  }
 };
 
 /*
