8c8a936f663db39bbe10d51beed4cd1d81c0abd7
SpiritReaver <54217513+SpiritReaver@users.noreply.github.com>
2022-04-25 20:45:08 -0500

    Update registro.component.ts

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Servicio/auth.service';
+import { DataBaseService } from 'src/app/Servicio/database.service';
 
 @Component({
-  selector: 'app-registro',
+  selector: 'app-register',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  usuarios: any;
+  usuario = {
+    email: '',
+    password: '',
+    name: ''
   }
+
+  ngOnInit() {
+    this.database.obtenerTodos("users").subscribe((usuariosRef) => {
+      console.log("usuariosRef: ", usuariosRef);
+      this.usuarios = usuariosRef.map(userRef => {
+        let usuario: any = userRef.payload.doc.data();
+        usuario['id'] = userRef.payload.doc.id;
+        return usuario;
+      });
+      console.log(this.usuarios)
+    })
+  }
+
+  constructor(private authService: AuthService, private database: DataBaseService, private router: Router) { }
+
+  registrarse() {
+    const { email, password } = this.usuario;
+    this.authService.register(email, password).then(user => {
+      console.log("se registro: ", user);
+      let lista = [...this.usuarios];
+      let existe = lista.find(user => user.email == email);
+
+      if (!existe) {
+        console.log("USUARIO NUEVO CREADO")
+        this.database.crear('users', this.usuario);
+      };
+
+      this.router.navigate(['/panelDeControl']);
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+
 
 }
