0198d0e468116d58785829e872fa2f5e84528010
SpiritReaver <54217513+SpiritReaver@users.noreply.github.com>
2022-04-25 20:44:57 -0500

    Update header.component.ts

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,6 +1,8 @@
 
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/Servicio/carrito.service';
+import { AuthService } from 'src/app/Servicio/auth.service';
+
 
 @Component({
   selector: 'app-header',
@@ -8,20 +10,26 @@ import { CartService } from 'src/app/Servicio/carrito.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+ 
   public totalItem : number = 0;
   public searchTerm !: string;
-  constructor(private cartService : CartService) { }
+  userLogged = this.authService.getUserLogged();
+  constructor(private cartService : CartService,private authService: AuthService) { }
 
   ngOnInit(): void {
     this.cartService.getProducts()
     .subscribe(res=>{
       this.totalItem = res.length;
     })
+    
   }
   search(event:any){
     this.searchTerm = (event.target as HTMLInputElement).value;
     console.log(this.searchTerm);
     this.cartService.search.next(this.searchTerm);
+  }
+
+  logOut(){
+    this.authService.logout();
   }
 }
