018c6b5d7a1954d7a21028e0f6d3a4156d010a7a
SpiritReaver <54217513+SpiritReaver@users.noreply.github.com>
2022-04-25 20:10:46 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarritoComponent } from './componentes/carrito/carrito.component';
 import { ProductoComponent } from './componentes/producto/producto.component';
+import {LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'productos',pathMatch:'full'},
   {path:'productos', component: ProductoComponent},
-  {path:'carrito', component: CarritoComponent}
+  {path:'carrito', component: CarritoComponent},
+  {path:'login', component: LoginComponent},
+  {path:'registro', component: RegistroComponent}
 ];
 
 @NgModule({
