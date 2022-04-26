c230b20417a4933b57eb1e42b45f14c9c7604a1e
SpiritReaver <54217513+SpiritReaver@users.noreply.github.com>
2022-04-25 20:45:02 -0500

    Update login.component.ts

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Servicio/auth.service';
+import { DataBaseService } from 'src/app/Servicio/database.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  usuario = {
+    email: '',
+    password: '',
   }
 
+  ngOnInit() {
+ 
+  }
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  Ingresar() {
+    const { email, password } = this.usuario;
+    this.authService.login(email, password).then(user => {
+      console.log("Bienvenido ", user);
+      if(!user) {
+        alert("Datos incorrectos, si no tienes cuenta ,registrate!");
+        return;
+      };
+      this.router.navigate(['/panelDeControl'])
+    }).catch(err=>{
+      console.log(err)
+    })
+  }
+
+
+  logout() {
+    this.authService.logout();
+  }
 }
